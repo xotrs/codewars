742d117a7b2ae06dad4c4e3b16850d3643c28f7f
Lim Young Rok <xotrs1@naver.com>
2018-03-04 01:47:30 +0900

    Mumbling 추가.

---
+++ Mumbling.rb
@@ -0,0 +1,25 @@
+# My Solution
+def accum(s)
+  total_string = ""
+  for i in 0...s.length
+    total_string += s[i].upcase + s[i].downcase * i + "-"
+  end
+  total_string[0...total_string.length-1]
+end
+
+# Best Solution
+#
+# def accum(s)
+#   s.chars.each_with_index.map{ |c, i| c.upcase + c.downcase * i }.join('-')
+# end
+
+# Sample Test
+# Test.describe("accum") do
+#   Test.it("Basic tests") do
+#     Test.assert_equals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+#     Test.assert_equals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+#     Test.assert_equals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+#     Test.assert_equals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+#     Test.assert_equals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+#   end
+# end
